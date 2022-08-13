6d3ea387fc32e01b97fe85604228eaf5335d8203
JoelSmalls <joelokanta46@gmail.com>
2022-08-13 16:09:41 +0100

    [Version 1.12] UPDATE: Added datetime and a list of variables associated to the day and time. Added a list to have specific days and hours for the program to not run on. Added exit clause to quit program if not in use.

--- zoom_join_automatic.py
+++ zoom_join_automatic.py
@@ -1,12 +1,22 @@
 """
-Version 1.1
-- Added a print statement for health check
+Version 1.12
+- Added lists for conditional statement to assess if the day the program is activated on is the correct day. Task Scheduler runs program everyday so this counteracts wasted processes.
+- Added variables for the date and hour
+- Added datetime
+- Added exit clause to close program when not in use.
 """
 import schedule
 import time
 import webbrowser
+from datetime import datetime
 
 main_link = 'https://us02web.zoom.us/j/2751012834'
+today = datetime.today()
+day = today.strftime("%A")
+hour = time.strftime("%H")
+
+nondays = ['Monday', 'Wednesday', 'Saturday']
+activationhour = [11, 20]
 
 def open_link(link):
     webbrowser.open(link)
@@ -14,12 +24,19 @@ def open_link(link):
 def church_meeting():
     open_link(main_link)
 
-print("Program initiated, Device Healthy!")
-schedule.every().tuesday.at("20:45").do(church_meeting)
-schedule.every().thursday.at("20:45").do(church_meeting)
-schedule.every().friday.at("20:45").do(church_meeting)
-schedule.every().sunday.at("11:10").do(church_meeting)
+
+print(f'Today is {day}. The program has been initiated, Device Healthy!')
+schedule.every().tuesday.at("20:50").do(church_meeting)
+schedule.every().thursday.at("20:50").do(church_meeting)
+schedule.every().friday.at("20:50").do(church_meeting)
+schedule.every().sunday.at("11:20").do(church_meeting)
 
 while 1:
-    schedule.run_pending()
-    time.sleep(1)
+    if day in nondays or hour not in activationhour:
+        print(f'The day {day} and time {hour} is not a part of the schedule! Exiting program now!')
+        time.sleep(1)
+        exit()
+    else:
+        schedule.run_pending()
+        time.sleep(600)
+        exit()
